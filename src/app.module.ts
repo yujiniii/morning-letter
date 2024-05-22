71b778af8c1aee3d476eaa7c6d196d5a42136e9d
유진 <yujiniii@icloud.com>
2024-05-22 15:51:29 +0900

    chore: config 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PublicDataModule } from './public-data/public-data.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PublicDataModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
