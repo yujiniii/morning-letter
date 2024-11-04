31b36f058b05ae3072e4b0d65ac9c3f4640b0ac3
유진 <yujiniii@icloud.com>
2024-11-05 03:11:05 +0900

    feat: ejs 설정 및 템플릿 추가
    ejn 사용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // use ejs
+  app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
+  app.setViewEngine('ejs');
+
   await app.listen(3000);
 }
+
 bootstrap();
